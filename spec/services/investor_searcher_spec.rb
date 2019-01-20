972bbeccfdd4f131401e36c0429f4a0b96d186ab
Fernando Almeida <fernando@fernandoalmeida.net>
2019-01-20 18:47:49 -0300

    Support multiple filters on investors

---
+++ spec/services/investor_searcher_spec.rb
@@ -0,0 +1,111 @@
+describe InvestorSearcher do
+  it 'filters by status' do
+    matching_investor = create(:investor, status: Investor::ACTIVE)
+    _non_matching_investor = create(:investor, status: Investor::INACTIVE)
+    filter_params = {
+      '0' => {
+        'type' => 'status',
+        'operator' => 'equal',
+        'value' => matching_investor.status
+      }
+    }
+
+    result = described_class.new(
+      filter_params,
+      matching_investor.domain_country_context
+    ).call
+
+    expect(result.pluck(:id)).to eq([matching_investor.id])
+  end
+
+  it 'filters by category' do
+    matching_investor = create(:investor, category: Investor::ANGEL)
+    _non_matching_investor = create(:investor, category: Investor::ACCELERATOR)
+    filter_params = {
+      '0' => {
+        'type' => 'category',
+        'operator' => 'equal',
+        'value' => matching_investor.category
+      }
+    }
+
+    result = described_class.new(
+      filter_params,
+      matching_investor.domain_country_context
+    ).call
+
+    expect(result.pluck(:id)).to eq([matching_investor.id])
+  end
+
+  it 'filters by funding stage' do
+    matching_investor = create(:investor, stage: Investor::SEED)
+    _non_matching_investor = create(:investor, stage: Investor::IPO)
+    filter_params = {
+      '0' => {
+        'type' => 'stage',
+        'operator' => 'equal',
+        'value' => matching_investor.stage
+      }
+    }
+
+    result = described_class.new(
+      filter_params,
+      matching_investor.domain_country_context
+    ).call
+
+    expect(result.pluck(:id)).to eq([matching_investor.id])
+  end
+
+  it 'filters by multiple criteria' do
+    matching_investor1 = create(
+      :investor,
+      status: Investor::ACTIVE,
+      category: Investor::ANGEL,
+      stage: Investor::SEED
+    )
+    matching_investor2 = create(
+      :investor,
+      status: Investor::ACTIVE,
+      category: Investor::ANGEL,
+      stage: Investor::SEED
+    )
+    _non_matching_investor1 = create(
+      :investor,
+      status: Investor::INACTIVE,
+      category: Investor::ANGEL,
+      stage: Investor::SEED
+    )
+    _non_matching_investor2 = create(
+      :investor,
+      status: Investor::ACTIVE,
+      category: Investor::ACCELERATOR,
+      stage: Investor::SEED
+    )
+    filter_params = {
+      '0' => {
+        'type' => 'status',
+        'operator' => 'equal',
+        'value' => Investor::ACTIVE
+      },
+      '1' => {
+        'type' => 'category',
+        'operator' => 'contains',
+        'value' => Investor::ANGEL
+      },
+      '2' => {
+        'type' => 'stage',
+        'operator' => 'contains',
+        'value' => Investor::SEED
+      }
+    }
+
+    result = described_class.new(
+      filter_params,
+      matching_investor1.domain_country_context
+    ).call
+
+    expect(result.pluck(:id)).to match_array(
+      [matching_investor1.id, matching_investor2.id]
+    )
+  end
+end
