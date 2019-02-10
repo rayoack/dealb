459d29e61bb4eaf7f88b4fdbb7eb4c68e69e7516
Lucas Bordignon <bordi.lucas@gmail.com>
2019-02-10 20:33:35 -0200

    Order by deals count

--- app/services/investors/ranking.rb
+++ app/services/investors/ranking.rb
@@ -13,15 +13,25 @@ module Investors
     def call!
       Investor.active
               .joins(:deals)
-              .select(:investable_type, :investable_id, :amount_currency)
-              .select('SUM(amount_cents) as invested_capital')
               .group(:id, :amount_currency)
+              .order(order_criteria)
               .preload(:investable)
-              .order(options[:order])
+              .select(:id,
+                      :investable_type,
+                      :investable_id,
+                      :amount_currency)
+              .select('SUM(amount_cents) as invested_capital')
+              .select('COUNT(deals.id) as deals_count')
     end
 
     private
 
     attr_reader :options
+
+    def order_criteria
+      return 'deals_count DESC' if options[:order] == :deals
+
+      options[:order]
+    end
   end
 end
--- spec/services/investors/ranking_spec.rb
+++ spec/services/investors/ranking_spec.rb
@@ -3,10 +3,10 @@ describe Investors::Ranking do
 
   let(:options) { {} }
 
-  let(:deal_1) { create :deal, first_name: 'A' }
-  let(:deal_2) { create :deal, first_name: 'B' }
-  let(:deal_3) { create :deal, first_name: 'C' }
-  let(:deal_4) { create :deal, first_name: 'D' }
+  let(:person_1) { create :person, first_name: 'A' }
+  let(:person_2) { create :person, first_name: 'B' }
+  let(:person_3) { create :person, first_name: 'C' }
+  let(:person_4) { create :person, first_name: 'D' }
 
   let!(:investor_1) { create :investor, investable: person_1 }
   let!(:investor_2) { create :investor, investable: person_2 }
@@ -15,34 +15,62 @@ describe Investors::Ranking do
 
   context 'success' do
     before do
-      create :deal_investors, investor: investor_1,
-                              deal: create(:deal, amount_cents: 100)
+      # First investor
+      create :deal_investor, investor: investor_1,
+                             deal: create(:deal, amount_cents: 100)
 
-      create :deal_investors, investor: investor_2,
-                              deal: create(:deal, amount_cents: 200)
+      # Second investor
+      create :deal_investor, investor: investor_2,
+                             deal: create(:deal, amount_cents: 200)
+      create :deal_investor, investor: investor_2,
+                             deal: create(:deal, amount_cents: 200)
+      create :deal_investor, investor: investor_2,
+                             deal: create(:deal, amount_cents: 200)
 
-      create :deal_investors, investor: investor_3,
-                              deal: create(:deal, amount_cents: 300)
+      # Third investor
+      create :deal_investor, investor: investor_3,
+                             deal: create(:deal, amount_cents: 300)
+      create :deal_investor, investor: investor_3,
+                             deal: create(:deal, amount_cents: 300)
+      create :deal_investor, investor: investor_3,
+                             deal: create(:deal, amount_cents: 300)
+      create :deal_investor, investor: investor_3,
+                             deal: create(:deal, amount_cents: 300)
 
-      create :deal_investors, investor: investor_4,
-                              deal: create(:deal, amount_cents: 400)
+      # Fourth investor
+      create :deal_investor, investor: investor_4,
+                             deal: create(:deal, amount_cents: 400)
+      create :deal_investor, investor: investor_4,
+                             deal: create(:deal, amount_cents: 400)
     end
 
     context 'without order priority' do
       it '#call!' do
-        expect(subject.call!).to eq(
+        expect(subject.call!.map(&:id)).to eq(
           [
-            investor_1,
-            investor_2,
-            investor_3,
-            investor_4
+            investor_1.id,
+            investor_2.id,
+            investor_3.id,
+            investor_4.id
           ]
         )
       end
     end
 
-    context 'order by name'
-    context 'order by deals'
+    context 'order by deals' do
+      let(:options) { { order: :deals } }
+
+      it '#call!' do
+        expect(subject.call!.map(&:id)).to eq(
+          [
+            investor_3.id,
+            investor_2.id,
+            investor_4.id,
+            investor_1.id
+          ]
+        )
+      end
+    end
     context 'order by capital'
     context 'order by location'
   end
