f077d689cb3fac4889ce6fa931f4ab0aa50063ab
Lucas Bordignon <bordi.lucas@gmail.com>
2019-03-24 23:04:15 -0300

    Mobile option for investors ranking

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,8 @@ Rails.application.routes.draw do
     },
     controllers: {
       omniauth_callbacks: 'users/omniauth_callbacks',
-      registrations: 'users/registrations'
+      registrations: 'users/registrations',
+      sessions: 'users/sessions'
     }
   )
 
