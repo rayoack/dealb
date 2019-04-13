402877e8e18bd91b077bd32253dd023540f4b37d
Lucas Bordignon <bordi.lucas@gmail.com>
2019-04-13 17:46:52 -0300

    Adjust I18n for user sign_up confirmation email

--- app/mailers/devise/users_mailer.rb
+++ app/mailers/devise/users_mailer.rb
@@ -3,7 +3,8 @@ module Devise
     include Devise::Controllers::UrlHelpers
 
     helper :application
-    default from: "dont_reply@#{ENV.fetch('DEALBOOK_DOMAIN')}",
+
+    default from: "dontreply@#{ENV.fetch('DEALBOOK_MAILGUN_DOMAIN')}",
             template_path: 'devise/mailer'
 
     def confirmation_instructions(record, token, opts = {})
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,6 +105,7 @@ Rails.application.configure do
     host: ENV.fetch('DEALBOOK_DOMAIN')
   }
 
+  # Mailgun deliveries settings
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
     api_key: ENV.fetch('DEALBOOK_MAILGUN_API_KEY'),
