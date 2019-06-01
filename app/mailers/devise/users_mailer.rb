b791d600c2bee059f7e1396b9d8d1e8a3c599be3
Lucas Bordignon <bordi.lucas@gmail.com>
2019-06-01 15:56:23 -0300

    Setup assets_pipeline to be used with Mailers

--- app/mailers/devise/users_mailer.rb
+++ app/mailers/devise/users_mailer.rb
@@ -2,10 +2,11 @@ module Devise
   class UsersMailer < Devise::Mailer
     include Devise::Controllers::UrlHelpers
 
+    layout 'mailer'
+
     helper :application
 
-    default from: "dontreply@#{ENV.fetch('DEALBOOK_MAILGUN_DOMAIN')}",
-            template_path: 'devise/mailer'
+    default from: "dontreply@#{ENV.fetch('DEALBOOK_MAILGUN_DOMAIN')}"
 
     def confirmation_instructions(record, token, opts = {})
       super
