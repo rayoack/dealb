473346502a7827e3ff49dc1b169a0a2e1096db1f
Diego Carvalho <direisc@gmail.com>
2020-01-20 22:52:01 -0300

    global search don't show investors (unified vision entity)

--- app/services/global_searcher.rb
+++ app/services/global_searcher.rb
@@ -13,18 +13,17 @@ class GlobalSearcher
     companies = Company.where('name ILIKE ?', "%#{search}%")
     people_by_first_name = Person.where('first_name ILIKE ?', "%#{search}%")
     people_by_last_name = Person.where('last_name ILIKE ?', "%#{search}%")
-    investor_by_company = Investor.company(companies.select(:id))
-    investor_by_person = Investor.people(people_by_first_name.pluck(:id) +
-                                         people_by_last_name.pluck(:id))
-    investor_by_tag = Investor.where('tag ILIKE ?', "%#{search}%")
+    #investor_by_company = Investor.company(companies.select(:id))
+    #investor_by_person = Investor.people(people_by_first_name.pluck(:id) + people_by_last_name.pluck(:id))
+    #investor_by_tag = Investor.where('tag ILIKE ?', "%#{search}%")
     deals_by_company = Deal.where(company_id: companies.select(:id))
 
     Array(people_by_first_name.to_a +
           people_by_last_name.to_a +
           companies.to_a +
-          investor_by_company.to_a +
-          investor_by_person.to_a +
-          investor_by_tag.to_a +
+          #investor_by_company.to_a +
+          #investor_by_person.to_a +
+          #investor_by_tag.to_a +
           deals_by_company.to_a).uniq
   end
 
