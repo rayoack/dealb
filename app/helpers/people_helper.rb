25e2e572c3a10bb12dbeaf1eb9914429114d94e1
Diego Carvalho <direisc@gmail.com>
2020-01-20 22:17:23 -0300

    people view when company is deleted

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -4,14 +4,13 @@ module PeopleHelper
   def latest_company(person, person_companies = person.person_companies)
     return t("empty_state") if person_companies.empty?
 
-    person_companies.map do |company|
-        link_to(company.company.name, "/companies/#{company.company.permalink}")
+    person_companies.map do |person_company|
+      link_to(person_company.company.name, "/companies/#{person_company.company.permalink}") if !person_company.company.blank?
     end.join(' / ').html_safe # rubocop:disable Rails/OutputSafety
   end
 
   def investor_category(person)
     return unless person.investor
-
     person.investor.tag
   end
 end
