b58f49c2b80046ada6240ef05d390a5c1ed19b88
direisc <direisc@gmail.com>
2020-01-31 21:21:29 -0300

    Update location for location dealbook.co style

--- app/services/import_old_database_service/import_location.rb
+++ app/services/import_old_database_service/import_location.rb
@@ -6,7 +6,7 @@ class ImportOldDatabaseService
       Rails.logger.info('-- location')
       ImportOldDatabaseService::Entities::Location.find_each do |location|
         printf('.')
-        ::Location.create!(country: location.country, city: location.city)
+        ::Location.create!(country: location.country, city: location.city, region: location.region)
       end
       Rails.logger.info("-- imported #{::Location.count} locations")
     end
@@ -14,8 +14,8 @@ class ImportOldDatabaseService
     def update
       Rails.logger.info('-- update location')
       ImportOldDatabaseService::Entities::Location.find_each do |location|
-        if !::Location.exists?(country: location.country, city: location.city)
-          ::Location.create!(country: location.country, city: location.city)
+        if !::Location.exists?(country: location.country, city: location.city, region: location.region)
+          ::Location.create!(country: location.country, city: location.city, region: location.region)
         end
       end
       Rails.logger.info('-- end update location')
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191228122953) do
+ActiveRecord::Schema.define(version: 20200201000747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "unaccent"
 
   create_table "companies", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +46,13 @@ ActiveRecord::Schema.define(version: 20191228122953) do
     t.index ["status"], name: "index_companies_on_status"
   end
 
+  create_table "company_locations", id: :serial, force: :cascade do |t|
+    t.bigint "company_id", null: false
+    t.bigint "location_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "company_markets", force: :cascade do |t|
     t.bigint "company_id", null: false
     t.bigint "market_id", null: false
@@ -52,6 +60,13 @@ ActiveRecord::Schema.define(version: 20191228122953) do
     t.datetime "updated_at", null: false
     t.index ["company_id"], name: "index_company_markets_on_company_id"
     t.index ["market_id"], name: "index_company_markets_on_market_id"
+  end
+
+  create_table "country", id: false, force: :cascade do |t|
+    t.string "cc_fips", limit: 2
+    t.string "cc_iso", limit: 2
+    t.string "tld", limit: 3
+    t.string "country_name", limit: 100
   end
 
   create_table "deal_investors", force: :cascade do |t|
@@ -116,8 +131,9 @@ ActiveRecord::Schema.define(version: 20191228122953) do
   create_table "locations", force: :cascade do |t|
     t.string "country", null: false
     t.string "city", null: false
-    t.datetime "created_at", null: false
+    t.datetime "created_at", default: -> { "now()" }, null: false
     t.datetime "updated_at", null: false
+    t.string "region"
     t.index ["city"], name: "index_locations_on_city"
     t.index ["country"], name: "index_locations_on_country"
   end
@@ -159,13 +175,21 @@ ActiveRecord::Schema.define(version: 20191228122953) do
   create_table "person_companies", force: :cascade do |t|
     t.bigint "person_id", null: false
     t.bigint "company_id", null: false
+    t.string "job_title"
     t.date "started_at"
     t.date "ended_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "job_title"
     t.index ["company_id"], name: "index_person_companies_on_company_id"
+    t.index ["job_title"], name: "index_person_companies_on_job_title"
     t.index ["person_id"], name: "index_person_companies_on_person_id"
+  end
+
+  create_table "person_locations", id: :serial, force: :cascade do |t|
+    t.bigint "person_id", null: false
+    t.bigint "location_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tags", force: :cascade do |t|
@@ -193,11 +217,11 @@ ActiveRecord::Schema.define(version: 20191228122953) do
     t.string "status", default: "active", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "person_id"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.bigint "person_id"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["person_id"], name: "index_users_on_person_id"
@@ -206,6 +230,12 @@ ActiveRecord::Schema.define(version: 20191228122953) do
     t.index ["role"], name: "index_users_on_role"
     t.index ["status"], name: "index_users_on_status"
     t.index ["uid"], name: "index_users_on_uid"
+  end
+
+  create_table "world_cities_free", id: false, force: :cascade do |t|
+    t.string "cc_fips", limit: 2
+    t.string "cc_iso", limit: 2
+    t.string "full_name_nd", limit: 200
   end
 
   add_foreign_key "deals", "users"
