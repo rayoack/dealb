4f37c161a17a5f42676333d7e2625a4a383a8eb1
direisc <direisc@gmail.com>
2020-01-31 22:29:26 -0300

    If mistake

--- app/services/import_old_database_service/import_investor_location.rb
+++ app/services/import_old_database_service/import_investor_location.rb
@@ -2,37 +2,26 @@
 
 class ImportOldDatabaseService
   class ImportInvestorLocation
-    # rubocop:disable Metrics/MethodLength
     def run
       @localizable_count_before = ::Localizable.count
-
-      ImportOldDatabaseService::Entities::InvestorLocation
-        .all.each do |investor_location|
-
+      ImportOldDatabaseService::Entities::InvestorLocation.all.each do |investor_location|
         printf('.')
-
         @investor_location = investor_location
-
         @old_location = Entities::Location.find(investor_location.location_id)
         @old_investor = Entities::Investor.find(investor_location.investor_id)
-
         @investable = (
           ::Person.find_by(permalink: @old_investor.slug.strip.presence) ||
           ::Company.find_by(permalink: @old_investor.slug.strip.presence)
         )
-
         @location = ::Location.find_by!(
           city: @old_location.city, country: @old_location.country
         )
-
         @localizable = ::Localizable.new(
           localizable: @investable,
           location: @location
         )
-
         @localizable.save!
       end
-
       Rails.logger.info("-- imported #{::Localizable.count} investor_locations")
     rescue StandardError
       raise
@@ -46,18 +35,16 @@ class ImportOldDatabaseService
         @old_investor = Entities::Investor.find(investor_location.investor_id)
         @person = ::Person.find_by(permalink: @old_investor.slug.strip.parameterize.presence)
         @company = ::Company.find_by(permalink: @old_investor.slug.strip.parameterize.presence)
-        
-        
         @location = ::Location.find_by!(
           city: @old_location.city, country: @old_location.country, region: @old_location.region
         )
 
-        @company.present? 
+        if @company.present? 
           if !::CompanyLocation.exists?(location: @location, company: @company)
             ::CompanyLocation.create!(location: @location, company: @company)
           end
         end
-        @person.present?
+        if @person.present?
           if !::PersonLocation.exists?(location: @location, person: @person)
             ::PersonLocation.create!(location: @location, person: @person)
           end
@@ -75,6 +62,5 @@ class ImportOldDatabaseService
       end
       Rails.logger.info('-- end update investor location')
     end
-    # rubocop:enable Metrics/MethodLength
   end
 end
